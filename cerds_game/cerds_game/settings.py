5b21e6b2486e52c3b3daebbf87ad0c58174ba535
Sviyatoslav Bentsa <112207138+Bentsa-S@users.noreply.github.com>
2025-04-15 16:33:07 +0200

    add STATIC_ROOT

--- cerds_game/cerds_game/settings.py
+++ cerds_game/cerds_game/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -161,5 +162,6 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
