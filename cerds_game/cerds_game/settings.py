22efae3f100ea4ba1296a495c50248f5ce7c8a3f
Bentsa-S <bentsa.sviatoslav@gmail.com>
2025-07-22 14:15:02 +0300

    caros

--- cerds_game/cerds_game/settings.py
+++ cerds_game/cerds_game/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:5174",
     "https://02ef-194-44-136-166.ngrok-free.app",
     config('URL_TO_OPEN_CROS', default='https://df41-194-44-136-166.ngrok-free.app')
 ]
